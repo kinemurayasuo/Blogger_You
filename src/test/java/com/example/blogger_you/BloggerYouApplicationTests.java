bee2d91aafc01b0108e20172bf6d31e7b35f6694
kinemurayasuo <fkdlzkf1@naver.com>
2024-02-17 19:33:32 +0900

    first commit

---
+++ src/test/java/com/example/blogger_you/BloggerYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogger_you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
